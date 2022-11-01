1516d77dadadcea02a1cd5231a882fa45c93092f
root <ferlps1@hotmail.com>
2022-11-01 23:15:31 +0000

    Script de Servidor Web

---
+++ script-iac.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo"Atulizando o servidor..."
+apt update
+apt upgrade -y
+
+apt install apache2 -y
+apt install unzip -y
+
+echo "Baixando os arquivos da aplicação..."
+cd /tmp/
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+
+
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
